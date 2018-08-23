e28f1fe0d77301f89f9940854bf785f5b2da8f5b
Canh <daongoccanh@gmail.com>
2018-08-24 00:39:18 +0700

    + first blood

---
+++ Assets/GSGameplay/Ball/SCR_Ball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_Ball : MonoBehaviour {
+	public const float BOUNCE_VELOCITY = 10;
+	public const float LIMIT_Y = 5.7f;
+
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		if (transform.position.y - Camera.main.transform.position.y < LIMIT_Y) {
+			float x = Camera.main.transform.position.x;
+			float y = transform.position.y - LIMIT_Y;
+			float z = Camera.main.transform.position.z;
+			Camera.main.transform.position = new Vector3(x, y, z);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		rb.velocity = new Vector2(0, BOUNCE_VELOCITY);
+    }
+}
---
+++ Assets/GSGameplay/SCR_Gameplay.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_Gameplay : MonoBehaviour {
+	public static float SCREEN_WIDTH;
+	public static float SCREEN_HEIGHT;
+
+	private GameObject[] boxes;
+	private Vector3[] startPositions;
+	private Vector3 mouseDownPosition;
+
+	// Use this for initialization
+	void Start() {
+		SCREEN_HEIGHT = Camera.main.orthographicSize * 2;
+		SCREEN_WIDTH = (float)Screen.width / Screen.height * SCREEN_HEIGHT;
+
+		boxes = GameObject.FindGameObjectsWithTag("Box");
+		startPositions = new Vector3[boxes.Length];
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			for (int i = 0; i < boxes.Length; i++) {
+				startPositions[i] = boxes[i].transform.position;
+				mouseDownPosition = Input.mousePosition;
+			}
+		}
+
+		if (Input.GetMouseButton(0)) {
+			for (int i = 0; i < boxes.Length; i++) {
+				float dx = Input.mousePosition.x - mouseDownPosition.x;
+				float x = startPositions[i].x + dx * SCREEN_WIDTH / Screen.width;
+				float y = startPositions[i].y;
+				float z = startPositions[i].z;
+				boxes[i].transform.position = new Vector3(x, y, z);
+			}
+		}
+	}
+}
